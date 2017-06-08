cd831ff7a631eb4e64ebd5d8fc276b57adb0cc26
ssj00@163.com <ssj00@163.com>
2017-06-08 10:47:53 +0800

    microservice-consumer-feign-hystrix-dashboard 项目

--- springCloudDemo/microservice-consumer-feign-hystrix-dashboard/src/main/java/com/shang/cloud/MicroserviceconsumerFeignHystrixDashboardApplication.java
+++ springCloudDemo/microservice-consumer-feign-hystrix-dashboard/src/main/java/com/shang/cloud/MicroserviceconsumerFeignHystrixDashboardApplication.java
@@ -2,18 +2,17 @@ package com.shang.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients
-@EnableCircuitBreaker
-public class MicroserviceconsumerFeignHystrixApplication {
+//@EnableEurekaClient
+//@EnableFeignClients
+@EnableHystrixDashboard
+public class MicroserviceconsumerFeignHystrixDashboardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceconsumerFeignHystrixApplication.class, args);
+		SpringApplication.run(MicroserviceconsumerFeignHystrixDashboardApplication.class, args);
 	}
 }
